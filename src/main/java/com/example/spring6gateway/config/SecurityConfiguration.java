40484623916ded84360a1075610669ad70d40930
Nicu Taban <nictbndeveloper@gmail.com>
2023-12-28 10:05:28 +0200

    Adds spring security configuration

---
+++ src/main/java/com/example/spring6gateway/config/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.example.spring6gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        http.csrf().disable()
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .oauth2ResourceServer().jwt();
+        return http.build();
+    }
+}
